0d1e1f3c9a86bab61890716a95a43d6a9b2e562b
Tomasz Bernaciak <t.bernaciak@binarapps.com>
2014-09-23 11:39:16 +0200

    sortable tables - finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
   def index
     @users = User.rank(:position).paginate(page: params[:page], per_page: 20)
+    @page = params[:page].nil? ? 1 : params[:page]
   end
 
   def sort
     @user = User.find(params[:id])
     @user.position = params[:position].to_i - 1 + (params[:page].to_i - 1) * 20
     respond_to do |format|
-      if @company.save!
+      if @user.save!
         format.json { head :ok }
       else
         format.json { head :error }
@@ -21,4 +22,8 @@ class UsersController < ApplicationController
     @users = User.gsub(/^/, '@').to_sym
     instance_variable_set(@users, User.rank(:position).all)
   end
+
+  def user_params
+      params.require(:user).permit(:name, :latname, :phone, :email, :position)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   include RankedModel
   ranks :position
+  after_save :update_position
   def update_position
     if self.position < 0
       self.position = 0
